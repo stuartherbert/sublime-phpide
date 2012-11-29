d114bd5e3d0d2326a0c4903317e243879fa9197e
Stuart Herbert <stuart@stuartherbert.com>
2012-11-29 15:33:30 +0000

    Initial commit, based on my only plugin list

---
+++ README.md
@@ -0,0 +1,33 @@
+# Sublime PHP IDE
+
+This plugin turns Sublime Text 2 into a fully-featured IDE for PHP development.
+
+## Installation
+
+Use Sublime Text 2's [Package Control](http://wbond.net/sublime_packages/package_control) (Preferences -> Package Control -> Install Package -> PHPIDE) to install this plugin.
+
+Once installed, this plugin will use Package Control to download and install the plugins that turn ST2 into a great development environment for PHP.
+
+## Plugins Installed
+
+This plugin uses Package Control to install the following plugins:
+
+* Additional PHP Snippets
+* Alignment
+* Bracket Highlighter
+* CTags
+* DocBlockr
+* Gist
+* Git
+* Goto Documentation
+* PHPUnit
+* Prefixr
+* Pretty JSON
+* SublimeCodeIntel
+* SublimeLinter
+* TrailingSpaces
+* WordCount
+
+(The full list is in the PHPIDE.sublime-settings file).
+
+If you know of any great PHP plugins that aren't on this list, send me a pull request :)
---
+++ phpide.py
@@ -0,0 +1,53 @@
+import os
+import sublime
+import sys
+import threading
+
+
+if os.name == 'nt':
+    from ctypes import windll, create_unicode_buffer
+
+
+class Prefs:
+    @staticmethod
+    def load():
+        settings = sublime.load_settings('PHPIDE.sublime-settings')
+        Prefs.plugins = settings.get('plugins', [])
+        Prefs.debug = settings.get('debug', 0)
+
+Prefs.load()
+
+
+def add_to_path(path):
+    # Python 2.x on Windows can't properly import from non-ASCII paths, so
+    # this code added the DOC 8.3 version of the lib folder to the path in
+    # case the user's username includes non-ASCII characters
+    if os.name == 'nt':
+        buf = create_unicode_buffer(512)
+        if windll.kernel32.GetShortPathNameW(path, buf, len(buf)):
+            path = buf.value
+
+    if path not in sys.path:
+        sys.path.append(path)
+
+# pull Package Control's files into our path
+pc_folder = os.path.join(sublime.packages_path(), 'Package Control')
+add_to_path(os.path.join(pc_folder, 'all'))
+
+# now we can load the Package Control code
+PackageControl = __import__('Package Control')
+
+
+class PHPIDE(threading.Thread):
+    def __init__(self):
+        self.manager = PackageControl.PackageManager()
+        threading.Thread.__init__(self)
+
+    def run(self):
+        installed_packages = self.manager.list_packages()
+        for plugin in Prefs.plugins:
+            if not plugin in installed_packages:
+                self.manager.install_package(plugin)
+
+
+# sublime.set_timeout(lambda: PHPIDE().start(), 3000)
